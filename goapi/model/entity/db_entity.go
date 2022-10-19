b9d326bd0e1d6157e366dbb43a293f622ef2785f
akane-mac <akane.watanabe05@gmail.com>
2022-10-19 20:23:52 +0900

    エンティティを追加

--- goapi/model/entity/db_entity.go
+++ goapi/model/entity/db_entity.go
@@ -16,3 +16,35 @@ type CafeEntity struct {
 	UpdatedAt     time.Time `json:"updated_at"`
 	DeletedAt     time.Time `json:"deleted_at"`
 }
+
+type UserEntity struct {
+	Id             int       `json:"id"`
+	Email          string    `json:"email"`
+	PasswordDigest string    `json:"password_digest"`
+	Nickname       string    `json:"nickname"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+}
+
+type ReviewEntity struct {
+	User_id   int       `json:"user_id"`
+	Cafe_id   int       `json:"cafe_id"`
+	Comment   string    `json:"comment"`
+	Rating    int       `json:"rating"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type FavoriteEntity struct {
+	User_id   int       `json:"user_id"`
+	Cafe_id   int       `json:"cafe_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type PrefectureEntity struct {
+	Id         int       `json:"id"`
+	Prefecture string    `json:"prefecture"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
