7934c39ad31b6a156a3a7d45ba4599e9fea2e04e
akane-mac <akane.watanabe05@gmail.com>
2022-10-19 20:40:57 +0900

    corsを設定

--- goapi/go.mod
+++ goapi/go.mod
@@ -3,9 +3,9 @@ module github.com/akane-05/cafekatu/goapi
 go 1.18
 
 require (
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/gorilla/mux v1.8.0
 	gorm.io/driver/mysql v1.4.1
 	gorm.io/gorm v1.24.0
 )
--- goapi/main.go
+++ goapi/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/akane-05/cafekatu/goapi/controller"
 	"github.com/akane-05/cafekatu/goapi/model/repository"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -25,6 +27,37 @@ func GetRouter() *gin.Engine {
 	log.Println("GetRouter")
 
 	r := gin.Default()
+
+	// ここからCorsの設定
+	r.Use(cors.New(cors.Config{
+		// アクセスを許可したいアクセス元
+		AllowOrigins: []string{
+			"http://localhost:3000",
+		},
+		// アクセスを許可したいHTTPメソッド(以下の例だとPUTやDELETEはアクセスできません)
+		AllowMethods: []string{
+			"POST",
+			"GET",
+			"DELETE",
+			"PUT",
+			"OPTIONS",
+		},
+		// 許可したいHTTPリクエストヘッダ
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+			"Access-Control-Allow-Origin",
+		},
+		// cookieなどの情報を必要とするかどうか
+		AllowCredentials: true,
+		// preflightリクエストの結果をキャッシュする時間
+		MaxAge: 24 * time.Hour,
+	}))
+
 	r.GET("/cafes", dc.GetCafes)
 	r.GET("/cafes/:id", dc.GetCafe)
 	r.POST("/cafes", dc.PostCafe)
