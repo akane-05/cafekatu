1ec3069284398a765a83dd5473cdd734dbb40014
akane-mac <akane.watanabe05@gmail.com>
2022-12-03 18:09:53 +0900

    レイアウトを調整

--- nextapp/src/components/elements/PastPostPaper.tsx
+++ nextapp/src/components/elements/PastPostPaper.tsx
@@ -4,6 +4,7 @@ import CafeInfoCard from '@/components/elements/CafeInfoCard'
 import CafeCard from '@/components/elements/CafeCard'
 import ReviewCard from '@/components/elements/ReviewCard'
 import { Review } from '@/features/reviews/types'
+import { Box } from '@mui/material'
 
 type Props = {
   pastPost: PastPost
@@ -11,11 +12,14 @@ type Props = {
 
 export default function PastPostPaper(props: Props) {
   return (
-    <CustomPaper>
-      <CafeCard cafeInfo={props.pastPost.cafeInfo}></CafeCard>
-      {props.pastPost.reviews.map((review: Review) => {
-        return <ReviewCard key={review.id} review={review} pastPost={true} /> //keyを指定
-      })}
-    </CustomPaper>
+    <>
+      <CustomPaper>
+        <CafeCard cafeInfo={props.pastPost.cafeInfo}></CafeCard>
+        {props.pastPost.reviews.map((review: Review) => {
+          return <ReviewCard key={review.id} review={review} pastPost={true} /> //keyを指定
+        })}
+      </CustomPaper>
+      <Box sx={{ mb: 2 }}></Box>
+    </>
   )
 }
